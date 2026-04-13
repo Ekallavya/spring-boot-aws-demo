cea2cde28d98bf530e50c67ccc6c7f0918a2913f
Ekallavya <madhureddie@gmail.com>
2026-04-13 17:04:59 +0530

    modified lombok methods

--- src/main/java/com/universe/opensky/dto/Product.java
+++ src/main/java/com/universe/opensky/dto/Product.java
@@ -5,14 +5,15 @@ import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor
+//@Data
+//@AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
+//@Getter
+//@Setter
+//@ToString
+//@EqualsAndHashCode
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Product implements Serializable {
@@ -21,5 +22,57 @@ public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
     private Long id;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(price, product.price) == 0 && Objects.equals(id, product.id) && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product(Long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     private double price;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
